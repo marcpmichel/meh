f98377327d00410c2bbf9dac48bf8a778380843b
Marc <marc@deepidoo.com>
2022-03-03 13:42:37 +0100

    WIP: removed underscore + added some widgets

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
 ui = {};
 ui.toolbar = { cols: [
-  { view:'label', label:'Title' },
-  { view:'button', label:'Click', click:function() { alert('clicked') } }
+  { view:'header', label:'Title' },
+  { view:'button', label:'Click' },
+  { view:'label', label: 'I am a label' }
 ]}
 
 ui.col2 = { view:'layout', rows: [
@@ -20,19 +21,18 @@ ui.col1 = { rows: [
 ]}
 
 
-ui.list = { view:'list', select: true,
+ui.list = { id:'list', view:'list', select: true,
   data:[ 
 		{id:1, value:"One"},
 		{id:2, value:"Two"},
 		{id:3, value:"Three"},
 		{id:4, value:"Four"},
 		{id:5, value:"Five"}
-	],
-	click: function() { console.log('click on list'); }
+	]
 }
 
 ui.col3 = { rows: [
-	{ view:'header', label: 'This is a list' },
+	{ id:'h1', view:'header', label: 'This is a list' },
 	ui.list
 ]}
 
@@ -40,9 +40,57 @@ ui.layout = { view:'layout', cols: [
   ui.col1, ui.col3, ui.col2
 ]}
 
+ui.menu = { id:'menu', view: 'menu', data: [
+   { id: 1, value:"Item 1" },
+   { id: 2, value:"Item 2" },
+   { id: 3, value:"Item 3" },
+]};
+
+ui.menutest = { rows: [
+  { view: 'header', label: 'This is a menu' },
+  ui.menu
+]};
+
+ui.tabs = { id:'tabs', view: 'tabs', tabs: [
+  { id: 'tab1', label: 'Tab1', view:'mv1' },
+  { id: 'tab2', label: 'Tab2', view:'mv2' },
+  { id: 'tab3', label: 'Tab3', view:'mv3' },
+]};
+
+
+/*
+ui.mv2 = { id: 'mv2', view:'layout', cols: [
+   { view:'label', label: 'View 2' }
+]};
+
+ui.mv3 = { id: 'mv3', view:'layout', cols: [
+  { view:'button', label: 'View 3' }
+]};
+*/
+
+ui.multiview = { id: 'multiview', view:'multiview', views: [
+  { id:'mv1', view:'template', template:'<span style="font-size: 14pt; background-color: red;">view 1</span>' },
+  { id:'mv2', view:'template', template:'<span style="font-size: 14pt; background-color: green;">view 2</span>' },
+  { id:'mv3', view:'template', template:'<span style="font-size: 14pt; background-color: blue;">view 3</span>' },
+]};
+
+ui.tabstest = { rows: [
+  { view: 'header', label: 'This is some tabs' },
+  ui.tabs,
+  ui.multiview
+]};
 
 meh.ready(function() {
-  meh.build(ui.layout)
+  meh.build({cols: [ui.col3, ui.menutest, ui.tabstest] })
+  meh.view('h1').on('click', function() { alert("clicked on header") });
+  meh.view('menu').on('itemclick', function(evt) { 
+    console.log("clicked on menu item " + evt.item_id); });
+  meh.view('list').on('itemclick', function(evt) {
+     console.log("clicked on list item " + evt.item_id); });
+  meh.view('tabs').on('itemclick', function(evt) {
+    console.log("clicked on tab " + evt.item_id); 
+    meh.view('multiview').select(evt.view_id);
+  });
 });
 
 
--- meh.js
+++ meh.js
@@ -8,15 +8,15 @@ meh.ready = function(fun, ...args) {
 }
 
 meh._views = {}
-meh._inits = {}
 
 meh.view = function(viewId) {
+  // console.log("seeking view " + viewId);
 	return meh._views[viewId]
 }
 
 meh.addView = function(conf={}, parentEl) {
     const el = document.createElement('div')
-    el.id = conf.id
+    if(conf.id) el.id = conf.id;
     el.className = "view " + conf.class
     el.innerHTML = conf.html || ""
     if(el.width) el.style += `flex 0 0 ${width}`
@@ -27,21 +27,8 @@ meh.addView = function(conf={}, parentEl) {
 
 meh.ui = {}
 meh.declare = function( name, ...args) {
-  // this._inits[name] = _.compact(_(args).reverse().map( o => o.init ))
-  // console.log(this._inits)
-  return meh.ui[name] = _.defaults.apply(this, args)
+  return meh.ui[name] = Object.assign({}, ...args.reverse())
 }
-
-/*
-meh.renderable = {
-  init: function(config) {
-    this._renderable = true
-  },
-  render: function(parentEl) {
-    this.el = meh.addView({html:'unkown view !'}, parentEl)
-  }
-}
-*/
 
 meh.eventable = {
 	_events: {},
@@ -57,7 +44,7 @@ meh.eventable = {
 		delete this._events[eventName]
   },
 	_none: function(eventName) {
-		_.keys(this._events).forEach( k => this.no(k) )
+    Object.keys(this._events).forEach( k => this.no(k))
 	}
 }
 
@@ -65,38 +52,43 @@ meh.data = {
 	_data: true
 }
 
+meh.uuid = function() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
 meh.constructView = function(viewDef, parentEl) {
 	if(!meh.ui[viewDef.view]) throw new Error(`"${viewDef.view}" : no such view`)
-	const v = _.clone(meh.ui[viewDef.view])
-	if(!viewDef.id) viewDef.id = _.uniqueId('meh')
+	const v = Object.create(meh.ui[viewDef.view]); // _.clone(meh.ui[viewDef.view])
+	if(!viewDef.id) viewDef.id = meh.uuid(); // _.uniqueId('meh')
 	if(meh._views[viewDef.id]) throw new Error(`view with id ${viewDef.id} already exist !`)
-
-	// console.log(viewDef.view, meh._inits[viewDef.view])
-	// if(meh._inits[viewDef.view]) meh._inits[viewDef.view].forEach( initFunc => initFunc.call(viewDef) )
+  
 	v.init(viewDef)
 	meh._views[viewDef.id] = v
+  // console.log(meh._views);
 	v.render(parentEl)
 	return v
 }
 
-meh.build=function(viewDef={}, parentEl=document.body) {
+meh.build = function(viewDef={}, parentEl=document.body) {
 
   if(viewDef.cols) {
-          const colEl = this.addView({class:'cols'}, parentEl)
-          viewDef.cols.forEach(function(def) {
-            meh.build(def, colEl)
-          })
+      const colEl = this.addView({id: viewDef.id, class:'cols'}, parentEl)
+      viewDef.cols.forEach(function(def) {
+        meh.build(def, colEl)
+      })
   }
   else if(viewDef.rows) {
-          const rowEl = this.addView({class:'rows'}, parentEl)
-          viewDef.rows.forEach(function(def) {
-            meh.build(def, rowEl)
-          })
+      const rowEl = this.addView({id: viewDef.id, class:'rows'}, parentEl)
+      viewDef.rows.forEach(function(def) {
+        meh.build(def, rowEl)
+      })
   }
   else if(viewDef.view) {
     const v = meh.constructView(viewDef, parentEl)
-		if(viewDef.click && _.isFunction(viewDef.click))
-			v.on('click', viewDef.click)
+		// if(viewDef.click && (typeof(viewDef.click) === 'function')) // _.isFunction(viewDef.click))
+		// 	v.on('click', viewDef.click)
   }
   else {
      // throw new Error(`don't know how to build ${JSON.stringify(ui)}`)
@@ -110,16 +102,20 @@ meh.declare("view", {
 		this.config = config
 		this.el = undefined
 		this.parentEl = undefined
+    this._views = {}
 	},
+  $addSubview: function(view) {
+    this._views[view.id] = view;
+  },
   render: function(parentEl) {
     this.el = meh.addView({html:'unkown view !'}, parentEl)
     this.parentEl = parentEl
   },
 	destroy: function() { 
     if(this.el) this.parentEl.removeChild(this.el)
-		if(this.eventable) this.none()
+		if(this._eventable) this.none()
   }
-} /* , meh.renderable */ )
+})
 
 meh.declare("template", {
   render: function(parentEl) {
@@ -147,7 +143,7 @@ meh.declare("header", {
 		this.el = meh.addView({html: this.config.label, class:"header" }, parentEl);
 		this.parentEl = parentEl;
 	}
-}, meh.ui.view)
+}, meh.eventable, meh.ui.view)
 
 meh.declare("list", {
   render: function(parentEl) {
@@ -156,13 +152,88 @@ meh.declare("list", {
 		var listEl = this.el;
 		if(this.config.data) {
 			const classes = this.config.select ? "item selectable" : "item";
-			this.config.data.forEach(function(item) {
-				itemView = meh.addView({html:item.value, class: classes}, listEl);
+      let item;
+			this.config.data.forEach((item) => {
+				item = meh.addView({id: item.id, html:item.value, class: classes}, listEl);
+        if(this.config.select) {
+          item.addEventListener('click', (evt) => {
+            const newevent = new Event('itemclick');
+            newevent.item_id = evt.target.id;
+            this.el.dispatchEvent(newevent);
+          });
+        }
 			});
 		}
   }
 }, meh.ui.view, meh.eventable)
 
+meh.declare("menu", {
+  render: function(parentEl) {
+    this.el = meh.addView({class:"menu"}, parentEl)
+    this.parentEl = parentEl;
+    if(this.config.data) {
+      const item_class = "menuitem selectable";
+      let item;
+      this.config.data.forEach((item) => {
+        item = meh.addView({id: item.id, html: item.value, class: item_class}, this.el);
+        item.addEventListener('click', (evt) => {
+          const newevent = new Event('itemclick');
+          newevent.item_id = evt.target.id;
+          this.el.dispatchEvent(newevent);
+        });
+      });
+    }
+  }
+}, meh.ui.view, meh.eventable);
+
+meh.declare("tabs", {
+  render: function(parentEl) {
+    this._tabs = [];
+    this.el = meh.addView({ class:"tabs"}, parentEl)
+    this.parentEl = parentEl;
+
+    if(this.config.tabs) {
+      this.current = this.config.tabs[0].id;
+      let item;
+      this.config.tabs.forEach((tab) => {
+        const classes = (tab.id == this.current) ? "tab tab-active" : "tab"; 
+        item = meh.addView({id: tab.id, html: tab.label, class: classes}, this.el);
+        item.addEventListener('click', (evt) => {
+          const newevent = new Event('itemclick');
+          newevent.item_id = evt.target.id;
+          newevent.view_id = tab.view; 
+          this.el.dispatchEvent(newevent);
+        });
+      });
+    }
+  }
+}, meh.ui.view, meh.eventable);
 
 
+meh.declare('multiview', {
+  render: function(parentEl) {
+    this.el = meh.addView({class:'multiview'}, parentEl);
+    this.parentEl = parentEl;
+    this.view_ids = [];
+
+    if(this.config.views) {
+      this.current_id = this.config.views[0].id;
+      let item;
+      this.config.views.forEach((view) => {
+        item = meh.constructView(view, this.el);
+        console.log(item);
+        this.view_ids.push(view.id);
+        if(view.id != this.current_id) item.el.style.display = 'none';
+      });
+    }
+  },
+  select(view_id) {
+      this.view_ids.forEach((vid) => {
+        let v = meh.view(vid);
+        if(v) {
+          v.el.style.display = (vid == view_id) ? 'block' : 'none';
+        }
+      });
+  }
+}, meh.ui.view);
 
